b3da63100a1e9cdec2d0ff381e7f0b5cfcb67005
Naarai Bernal <110580725+raibernal03@users.noreply.github.com>
2025-10-30 13:39:48 -0500

    lease contract

--- src/main/java/org/example/contracts/LeaseContract.java
+++ src/main/java/org/example/contracts/LeaseContract.java
@@ -1,5 +1,33 @@
 package org.example.contracts;
 
+import org.example.dealership.Vehicle;
+
 public class LeaseContract extends Contract {
 
+
+    public LeaseContract(String date, String customerName, String customerEmail, Vehicle vehicle) {
+        super(date, customerName, customerEmail, vehicle);
+    }
+
+    public double getExpectedEndingValue() {
+        return (getVehicle().getPrice() * 0.50);
+    }
+
+    public double getLeaseFee() {
+        return (getVehicle().getPrice() * 0.07);
+    }
+
+    @Override
+    public double getTotalPrice() {
+        return (getVehicle().getPrice() - getExpectedEndingValue()) + getLeaseFee();
+    }
+
+    @Override
+    public double getMonthlyPayment() {
+        double rate = 0.04;
+        int months = 36;
+        double top = getTotalPrice() * (rate / 12);
+        double bottom = 1 - (Math.pow((1 + (rate / 12)), -months));
+        return top / bottom;
+    }
 }
--- src/main/java/org/example/contracts/SalesContract.java
+++ src/main/java/org/example/contracts/SalesContract.java
@@ -10,7 +10,7 @@ public class SalesContract extends Contract {
         this.financeOrNot = financeOrNot;
     }
 
-    //getters & setters
+    //getters
     public double getSalesTaxAmount() {
         return getVehicle().getPrice() * 0.05;
     }
@@ -34,32 +34,32 @@ public class SalesContract extends Contract {
     public void setFinanceOrNot(boolean financeOrNot) {
         this.financeOrNot = financeOrNot;
     }
+    @Override
+    public double getTotalPrice() {
+        return (getVehicle().getPrice() + getSalesTaxAmount() + getRecordingFee() + getProcessingFee());
+    }
 
     @Override
     public double getMonthlyPayment() {
-        double p = getVehicle().getPrice();
+        double p = getTotalPrice();
         double r = 0;
         double n = 0;
         if (!financeOrNot) {
             return 0;
         } else {
             if (p >= 10000) {
-                r = .0425;
+                r = .0425 / 12;
                 n = 48;
 
             } else {
-                r = 0.0525;
+                r = 0.0525 / 12;
                 n = 24;
             }
-            double top = p * r * (Math.pow((1 + r), n));
+            double top = r * (Math.pow((1 + r), n));
             double bottom = (Math.pow((1 + r), n)) - 1;
-            return top / bottom;
+            double solution = p * (top / bottom);
+            return solution;
         }
-    }
-
-    @Override
-    public double getTotalPrice() {
-        return 0;
     }
 
 }
